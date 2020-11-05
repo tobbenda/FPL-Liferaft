a458833bd4913ab28e7356b4f6831fd25988f08c
torbjorn dahl <torbjorn@host-78-65-176-153.homerun.telia.com>
2020-11-05 23:48:10 +0100

    Getting better

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import Footer from './components/Footer/Footer';
 import SettingsBox from './components/SettingsBox/SettingsBox';
 import DataBox from './components/DataBox/DataBox';
 import React, { createContext, useState } from 'react';
+import cover from './fpl-cover-foto-01.jpg';
+import cover_cropped from './fpl-cover-cropped.jpg';
 
-const sortOptions = [
+const sortOptions_raw = [
   { attributeID: "web_name", prettyName: "Short Name", checked: true },
   { attributeID: "total_points", prettyName: "Total Points", checked: true },
   { attributeID: "first_name", prettyName: "First name", checked: false },
@@ -57,6 +59,48 @@ const sortOptions = [
   { attributeID: "position", prettyName: "Position", checked: true },
 ]
 
+const sortOptions = [
+  { attributeID: "web_name", prettyName: "Short Name", checked: true },
+  { attributeID: "total_points", prettyName: "Total Points", checked: true },
+  { attributeID: "first_name", prettyName: "First name", checked: false },
+  { attributeID: "second_name", prettyName: "Second name", checked: true },
+  { attributeID: "points_per_game", prettyName: "Points pr game", checked: true },
+  { attributeID: "form", prettyName: "Form", checked: true },
+  { attributeID: "now_cost", prettyName: "Current Price", checked: true },
+  { attributeID: "points_pr_mill", prettyName: "Points pr mill", checked: true },
+  { attributeID: "points_pr_game_pr_mill", prettyName: "Points pr game pr mill", checked: true },
+  { attributeID: "team_name", prettyName: "Team", checked: true },
+  { attributeID: "position", prettyName: "Position", checked: true },
+  { attributeID: "goals_scored", prettyName: "Goals scored", checked: false },
+  { attributeID: "assists", prettyName: "Assists", checked: false },
+  { attributeID: "clean_sheets", prettyName: "Clean sheets", checked: false },
+  { attributeID: "minutes", prettyName: "Minutes played", checked: true },
+  { attributeID: "cost_change_start", prettyName: "Total price change", checked: false },
+  { attributeID: "event_points", prettyName: "Points last GW", checked: false },
+  { attributeID: "goals_conceded", prettyName: "Goals conceded", checked: false },
+  { attributeID: "news", prettyName: "News", checked: true },
+  { attributeID: "selected_by_percent", prettyName: "Selected by percent", checked: false },
+  { attributeID: "yellow_cards", prettyName: "Yellow cards", checked: false },
+  { attributeID: "red_cards", prettyName: "Red cards", checked: false },
+  { attributeID: "bonus", prettyName: "Bonus Points", checked: false },
+  { attributeID: "penalties_saved", prettyName: "Penalties saved (Goalkeeper)", checked: false },
+  { attributeID: "penalties_missed", prettyName: "Penaltes missed", checked: false },
+  { attributeID: "saves", prettyName: "Saves (Goalkeeper)", checked: false },
+  { attributeID: "ict_index", prettyName: "ICT-Index", checked: false },
+  { attributeID: "influence_rank", prettyName: "Influence rank", checked: false },
+  { attributeID: "influence_rank_type", prettyName: "Influence rank position", checked: false },
+  { attributeID: "creativity_rank", prettyName: "Creativity rank", checked: false },
+  { attributeID: "creativity_rank_type", prettyName: "Creativity rank position", checked: false },
+  { attributeID: "threat_rank", prettyName: "Threat rank", checked: false },
+  { attributeID: "threat_rank_type", prettyName: "Threat rank position", checked: false },
+  { attributeID: "ict_index_rank", prettyName: "ICT rank", checked: false },
+  { attributeID: "ict_index_rank_type", prettyName: "ICT rank position", checked: false },
+  { attributeID: "transfers_in_event", prettyName: "GW transfers in", checked: false },
+  { attributeID: "transfers_out_event", prettyName: "GW trannsfers out", checked: false },
+  { attributeID: "value_form", prettyName: "Value form (?)", checked: false },
+  { attributeID: "status", prettyName: "Status", checked: false },
+]
+
 export const myContext = createContext();
 
 function App() {
@@ -64,7 +108,6 @@ function App() {
   const [checkValues, setCheckValues] = useState(sortOptions);
   const [posFilter, setPosFilter] = useState('All');
   const [sortBy, setSortBy] = useState('total_points');
-  console.log(checkValues);
 
   const getCheckedAttributesQuery = () => {
     const checkedAttributes = checkValues.filter(el => el.checked === true).map(attr => attr.attributeID).join(' ');
@@ -116,6 +159,8 @@ function App() {
      } >
       <div className="App">
         <Header />
+        <img className="cover-img-cropped" alt='football players' src={cover_cropped}></img>
+        <img className="cover-img" alt='football players' src={cover}></img>
         <SettingsBox />
         <DataBox />
         <Footer />
